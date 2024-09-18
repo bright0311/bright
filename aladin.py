ebe25480615e5d1724ee2fe44d70f043e8c92437
bright0311 <brightson7170@gmail.com>
2024-09-18 12:45:39 +0530

    PYTHON

---
+++ aladin.py
@@ -0,0 +1,9 @@
+
+import math
+
+def area_of_circle(radius):
+    return math.pi * radius ** 2
+
+radius = float(input("Enter the radius of the circle: "))
+area = area_of_circle(radius)
+print(f"The area of the circle is: {area:.2f}")
